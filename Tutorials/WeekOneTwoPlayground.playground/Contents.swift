4e880ae2895e21087aae3053fea3832a49ca6550
Dilum De Silva <dilumdesilva.info@gmail.com>
2021-01-30 10:40:03 +0530

    add: week one and two playground

---
+++ Tutorials/WeekOneTwoPlayground.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+
+var student = "dilum"
+let id: Int = 1234
+var enrolledClass: String = "iOS"
+
+let studentDetails =  "Name: \(student), id: \(id)"
+
+print(studentDetails)
+
+var variableNumber: Int = 42
+variableNumber = 0
+variableNumber = 1_000_000
+
+let myself="""
+I am a software engineer
+who's working for circle.life
+id: \(id)
+
+"""
+var integer: Int = 100
+var decimal: Double = 12.5
+integer = Int(decimal)
+
+let coordinates: (Double, Double) = (2, 3)
+
+
+let x: Double = coordinates.0
+
+let coordinates3D: (x: Int, y: Int, z: Int) = (2, 3, 1)
+
+let (a,b,_) = coordinates3D
+
+
+let stuIDList =  [1,2,3,4]
+var stuID = 0
+
+for stuID in stuIDList {
+    if stuID == 2{
+        print
+    }
+}
