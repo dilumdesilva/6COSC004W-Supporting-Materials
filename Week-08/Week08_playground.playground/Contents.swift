f75633b966ceb0ed5f0c533d603c028b0f519067
Dilum De Silva <dilum.desilva@circles.asia>
2021-03-13 02:27:50 +0530

    week-08: playground for core graphics data structures

---
+++ Week-08/Week08_playground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Hello, Week-08"
+
+//Cordinate system data structures
+
+//CGPoint to mark 2D space
+var cordinates = CGPoint(x: 91.0, y: 70.5)
+
+//CGSize to define shapes with height and width
+var rectangleDim = CGSize(width: 200.0, height: 100.5)
+
+//CGRect to define a rectangle shape
+let reactangle = CGRect(origin: cordinates, size: rectangleDim)
